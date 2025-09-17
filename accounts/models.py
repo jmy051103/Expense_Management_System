2f5e95d013b000260a050dd10e199e2f6800bd65
Jiwon Hong <jiwonhong37@gmail.com>
2025-09-17 22:20:39 +0900

    added more choices for edit_account

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    ROLE_CHOICES = [("사장", "사장"), ("부장", "부장"), ("팀장", "팀장")]
-    DEPT_CHOICES = [("홍보팀", "홍보팀"), ("기획팀", "기획팀"), ("회장단", "회장단")]
-    ACCESS_CHOICES = [("관리자모드", "관리자모드"), ("사장모드", "사장모드"), ("직원모드", "직원모드")]
+    ROLE_CHOICES = [("대표이사", "대표이사"), ("실장", "실장"), ("부장", "부장"), ("과장", "과장"), ("대리", "대리"), ("주임", "주임"), ("사원", "사원"), ("차장", "차장")]
+    DEPT_CHOICES = [("연구개발전담부(디자인팀)", "연구개발전담부(디자인팀)"), ("MD팀(제품기획및개발)", "MD팀(제품기획및개발)"), ("영업팀", "영업팀"), ("회계팀", "회계팀"), ("물류팀", "물류팀")]
+    ACCESS_CHOICES = [("관리자모드", "관리자모드"), ("사장모드", "사장모드"), ("직원모드", "직원모드"), ("실장모드", "실장모드")]
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
